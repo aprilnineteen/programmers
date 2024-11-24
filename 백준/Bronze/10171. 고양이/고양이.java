fd5e1746626a9f8bb3cd5ee4f3971201975e6a99
덴코 <154617066+aprilnineteen@users.noreply.github.com>
2024-11-24 15:11:32 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,15 @@
+import static java.lang.System.out;
+
+public class Main {
+    private static final String CAT_EARS = "\\    /\\\n";
+    private static final String CAT_FACE = " )  ( ')\n";
+    private static final String CAT_BODY = "(  /  )\n";
+    private static final String CAT_TAIL = " \\(__)|\n";
+
+    public static void main(String[] args) {
+        out.print(CAT_EARS);
+        out.print(CAT_FACE);
+        out.print(CAT_BODY);
+        out.print(CAT_TAIL);
+    }
+}
